1c52fc4f44190660f6736c8f7c4352fb46946f64
Duke82 <dukes.mailbox@gmail.com>
2014-06-08 14:18:19 -0700

    Changes DB Name to bm

--- bm/bm/settings.py
+++ bm/bm/settings.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'bm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'bm'),
     }
 }
 
